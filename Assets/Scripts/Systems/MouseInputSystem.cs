f8e5d40776b0415ed38dc477a142a6d091e14b87
Alexander Shust <alexander.shust@developersunknown.com>
2022-09-21 07:16:27 +0300

    First working system

---
+++ Assets/Scripts/Systems/MouseInputSystem.cs
@@ -0,0 +1,20 @@
+﻿using Components;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Systems
+{
+    public class MouseInputSystem : IEcsRunSystem
+    {
+        public void Run(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+            if (Input.GetMouseButtonUp(0))
+            {
+                var entity = world.NewEntity();
+                var buttons = world.GetPool<MouseButtonPressed>();
+                buttons.Add(entity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using Components;
+using Leopotam.EcsLite;
+using UnityEngine;
+
+namespace Systems
+{
+    public class PlayerMovementSystem : IEcsRunSystem
+    {
+        public void Run(IEcsSystems systems)
+        {
+            var world = systems.GetWorld();
+            var filter = world.Filter<MouseButtonPressed>().End();
+            var pool = world.GetPool<MouseButtonPressed>();
+            foreach (var entity in filter)
+            {
+                Debug.LogError("Mouse button pressed");
+                pool.Del(entity);
+            }
+        }
+    }
+}
